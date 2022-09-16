45430b8402cd6045e4951408af4f1136a3c6bf9c
jc.oh <joh@servicenation.com>
2022-09-16 10:40:01 -0500

    Section 4: Starbase | Application Two

--- src/App.vue.js
+++ src/App.vue.js
@@ -2,13 +2,32 @@ const templete = `
 <div>
     <div class="container">
         <div class="row">
-            <div class="col">
-            <h3>{{title}}</h3>
-            {{message}}<br>
-            <button class="btn btn-primary" v-on:click="fetchCharactor()">Test fetch Charactor</button>
-            <hr>
-            <Charactor></Charactor>
+            <div class="col">            
+<pre style="color:#d1d1d1;background:#000000;">http://swapi<span style="color:#00a800; ">.</span>dev/api/
+</pre>
+
+<pre style="color:#d1d1d1;background:#000000;"><span style="color:#ff8906; ">&lt;</span><span style="color:#e66170; font-weight:bold; ">script</span> src<span style="color:#d2cd86; ">=</span><span style="color:#00c4c4; ">"./src/main.js"</span> type<span style="color:#d2cd86; ">=</span><span style="color:#00c4c4; ">"module"</span><span style="color:#ff8906; ">&gt;</span><span style="color:#ff8906; ">&lt;/</span><span style="color:#e66170; font-weight:bold; ">script</span><span style="color:#ff8906; ">&gt;</span>
+</pre>
+
+
             </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>{{title}}</h3>
+                {{message}}<br>
+                <button class="btn btn-primary" v-on:click="fetchCharactor()">Test fetch Charactor</button>
+                <hr>
+                <charactor v-bind:id="1"></charactor>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <charactor 
+                v-for="(id, seq) in initial_ids"
+                :key="seq"
+                :id="id"
+            />
         </div>
     </div>
 </div>
@@ -18,11 +37,12 @@ export default {
     name: 'app',
     template: templete,
     components: {
-        Charactor
+        charactor: Charactor 
     },
     data() {
         return {
             title: "Generate Your Team",
+            initial_ids: [1, 13, 14, 25],
             message: "Hello World-1"
         }
     },
--- src/components/Charactor.vue.js
+++ src/components/Charactor.vue.js
@@ -1,26 +1,44 @@
 const template = `
-<div>
-{{charactor}}
+<div class="col-4 m-1">
+    <div class="card character-card ">
+        <div class="card-header"><h4 class="card-title">{{charactor.name}}</h4></div>
+        <div class="card-body">
+            
+            <p class="card-text">Hieght: {{charactor.height}}cm</p>
+            <p class="card-text">Mass: {{charactor.mass}}kb</p>
+            <p class="card-text">Hair Color: {{charactor.hair_color}}</p>
+            <p class="card-text">Eye Color: {{charactor.eye_color}}kb</p>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-info" @click="switchCharactor">Switch Charactor</button>
+        </div>
+    </div>
+<!--{{charactor}}-->
 </div>
 `
 export default {
     name: 'charactor',
     template: template,
+    props: ['id'],
     data() {
         return {
             charactor: {}
         }
     },
     created() {
-        this.fetchCharactor()
+        this.fetchCharactor(this.id)
     },
     methods: {
-        fetchCharactor() {
-            fetch('http://swapi.dev/api/people/1', {
+        fetchCharactor(id) {
+            fetch(`http://swapi.dev/api/people/${id}`, {
                 method: 'GET'
             })
                 .then(response => response.json())
                 .then(json => this.charactor = json)
+        },
+        switchCharactor() {
+            let random_id = Math.floor(Math.random() * 83) + 1
+            this.fetchCharactor(random_id)
         }
     }
 }
